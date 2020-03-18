ee7ea6ab6ff4a4f16b883e6f591cb3a15e3e3068
vishak <vishakpviswanath56@gmail.com>
2020-03-18 16:53:48 +0530

    created form and authentication page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
 
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login_page.html'), name='login'),
+    path('new_home/', views.home, name='home'), 
 ]
