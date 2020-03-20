63283d62285035c7a73034807591639113662487
vishak <vishakpviswanath56@gmail.com>
2020-03-21 00:18:03 +0530

    New theme for the site

--- blog/urls.py
+++ blog/urls.py
@@ -8,10 +8,12 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
-    path('', views.post_list, name='post_list'),
+    path('',views.first_page, name='first_page'),
+    path('post/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login_page.html'), name='login'),
     path('new_home/', views.home, name='home'), 
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('register/', views.register, name='register' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,13 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
+def first_page(request):
+      return render(request, 'blog/first_page.html')
+
 def post_list(request):
       posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')   
       return render(request, 'blog/post_list.html', {'posts': posts})
@@ -49,3 +53,18 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def register(request):
+      if request.method =='POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                  form.save()
+                  return redirect('post_list')
+
+      else:
+            form = UserCreationForm()
+
+            args = {'form':form}
+            return render(request, 'blog/reg_form.html', args)      
+
