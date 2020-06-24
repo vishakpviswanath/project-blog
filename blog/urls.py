cd51f43072d4b39ef2833b7ea3abb97d83916680
vishak <vishakpviswanath56@gmail.com>
2020-06-24 20:33:44 +0530

    new modification

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login_page.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/first_page.html'), name='logout'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('register/', views.register, name='register' ),
     path('profile/', views.profile, name='profile'),
